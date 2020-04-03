5c7f24bd8fab52f717345d67bc0a3fd25d504f77
muhammad f huda <52238215+muhammad-f-huda@users.noreply.github.com>
2020-04-03 15:20:57 +0700

    import Buffer

--- src/utils.ts
+++ src/utils.ts
@@ -3,6 +3,9 @@
  * @param obj - The object to flip
  * @returns The flipped object
  */
+
+import { Buffer } from 'buffer';
+
 export const flipObject = <keyType extends string, valueType extends string>(obj: {[key in keyType]: valueType}) => {
 	return Object.fromEntries(Object.entries(obj).map(([ key, value ]) => [ value, key ])) as {[key in valueType]: keyType};
 };
